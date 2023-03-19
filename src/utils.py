2d0804db727dcd7481bec8cf24b302c61eb0614b
prasant poudel <prasantpoudel@prasants-Air.bbrouter>
2023-03-19 21:59:49 +0530

    hyperparameter tunning

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 from src.logger import logging
@@ -19,16 +20,24 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,params):
     try:
         
         report={}
         logging.info("model is training")
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para=params[list(models.keys())[i]]
             
 
+            # model.fit(x_train,y_train)
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
+
             y_train_pred=model.predict(x_train)
             y_test_pred=model.predict(x_test)
 
