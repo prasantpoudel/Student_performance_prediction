ad27a5c2e501faaf1cba2e08af6e7b9ffd36f6cd
prasant poudel <prasantpoudel@prasants-Air.bbrouter>
2023-03-20 17:58:31 +0530

    deployment stage

--- application.py
+++ application.py
@@ -38,6 +38,6 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
 
 if __name__=="__main__":
-    app.run(debug=True, port=8001)
+    app.run(host="0.0.0.0")
 
 
